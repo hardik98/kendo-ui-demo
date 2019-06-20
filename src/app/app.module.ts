7b9a647663fd788a645bea093e2fa664c1f1bfb8
hardik thakkar <hardikhalnai1998@gmail.com>
2019-06-20 17:26:23 +0530

    commited

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { KendobuttonComponent } from './kendobutton/kendobutton.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    KendobuttonComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ButtonsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/kendobutton/kendobutton.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: '',
+  templateUrl: './kendobutton.component.html',
+  styleUrls: ['./kendobutton.component.css']
+})
+export class KendobuttonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
